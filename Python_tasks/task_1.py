a76b9a3d4d2570b88a5e75bd2900c1c006bb7d30
Tanya <tayasinha1514@gmail.com>
2025-08-16 00:39:14 +0530

    Added task 1 annd task 2 of assignment 1

---
+++ Python_tasks/task_1.py
@@ -0,0 +1,17 @@
+# Perform Basic Mathematical Operations
+
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Performing operations
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+# Displaying results
+print("\nAddition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
---
+++ Python_tasks/task_2.py
@@ -0,0 +1,6 @@
+# Create a Personalized Greeting
+
+a=str(input("Enter your First name: "))
+b=str(input("Enter your Second name: "))
+
+print('Hello,', a, b,'! welcome to the python program.')
