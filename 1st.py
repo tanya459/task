424c9598af617f765a519456c8701781131aeee1
Tanya <tayasinha1514@gmail.com>
2025-08-15 11:56:51 +0530

    Initial commit

---
+++ 1st.py
@@ -0,0 +1,20 @@
+# print("hello world")
+# a=input("enter a value ")
+# print(a)
+
+# c=input("calsius : ")
+# c=float(c)
+# f=(c*9/5)+32
+# print("farnheit: ",f)
+
+# p=int(input('principle : '))
+# r=int(input('rate :'))
+# t=int(input('time :'))
+# si=(p*r*t)/100
+# print('simple interest = ',si)
+
+
+print(max(1,2,5,7,6))
+print(min(3,5,8,5,1,8,1))
+print(abs(-4586))
+print(pow(5,9))
